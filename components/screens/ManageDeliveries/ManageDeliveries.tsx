6d48d4b54d48aaf17191c302718bf97b33670dd3
Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>
2021-01-25 16:29:21 +0200

    Update components/screens/ManageDeliveries/ManageDeliveries.tsx

--- components/screens/ManageDeliveries/ManageDeliveries.tsx
+++ components/screens/ManageDeliveries/ManageDeliveries.tsx
@@ -311,7 +311,7 @@ class ManageDeliveries extends React.Component<Props, State> {
         }
         <TouchableOpacity
           style={[styles.begindeliveryButton, { width: "70%", height: 60, marginTop: 25, alignSelf: "center" }]}
-          onPress={() => this.handleListItemPress(category, true, undefined)}
+          onPress={ () => this.handleListItemPress(category, true, undefined) }
         >
           <Text style={{ color: '#f2f2f2', fontWeight: "600" }}>Uusi toimitus</Text>
         </TouchableOpacity>
