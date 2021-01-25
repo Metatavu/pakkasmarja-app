21896b04b3b753338ca3fa151bd7b0847bf76017
Tero Lamminpaa <tero.lamminpaa@gmail.com>
2021-01-25 15:48:29 +0200

    Selected date is now passed to modal as navigation parameter

--- components/screens/deliveries/EditDelivery.tsx
+++ components/screens/deliveries/EditDelivery.tsx
@@ -2,7 +2,7 @@ import React, { Dispatch } from "react";
 import { connect } from "react-redux";
 import BasicScrollLayout from "../../layout/BasicScrollLayout";
 import TopBar from "../../layout/TopBar";
-import { AccessToken, StoreState, DeliveriesState, DeliveryProduct, DeliveryDataKey, DeliveryNoteData } from "../../../types";
+import { AccessToken, StoreState, DeliveriesState, DeliveryProduct } from "../../../types";
 import * as actions from "../../../actions";
 import { View, ActivityIndicator, Picker, TouchableOpacity, TouchableHighlight, Platform, Dimensions, Alert } from "react-native";
 import Api, { Delivery, Product, DeliveryNote, DeliveryPlace, ItemGroupCategory, ProductPrice, OpeningHourInterval } from "pakkasmarja-client";
@@ -19,8 +19,6 @@ import { extendMoment } from "moment-range";
 import _ from "lodash";
 import CreateDeliveryNoteModal from "./CreateDeliveryNoteModal";
 import ViewOrDeleteNoteModal from "./ViewOrDeleteNoteModal";
-import DateTimePicker from "react-native-modal-datetime-picker";
-import EntypoIcon from "react-native-vector-icons/Entypo";
 import { roundPrice } from "../../../utils/utility-functions";
 
 const Moment = require("moment");
@@ -675,7 +673,7 @@ class EditDelivery extends React.Component<Props, State> {
         <View style={{ flex: 1.1 }}>
           { productPrice ?
             <Text style={ styles.textPrediction }>
-              {`Tämän hetkinen hinta ${productPrice.price} € / ${productPrice.unit.toUpperCase()} ALV 0% (${roundPrice(parseFloat(this.state.productPrice.price) * 1.14)})`}
+              {`Tämän hetkinen hinta ${productPrice.price} € / ${productPrice.unit.toUpperCase()} ALV 0% (${roundPrice(parseFloat(productPrice.price) * 1.14)})`}
             </Text> :
             <Text style={ styles.textPrediction }>
               {`Tuotteelle ei löydy hintaa`}
