862da14bdf9c257b16fbdb62922d5ec1349228e0
Tero Lamminpää <56538990+terolamminpaa@users.noreply.github.com>
2021-04-21 08:26:52 +0300

    Update components/screens/deliveries/ViewAllDeliveriesScreen.tsx
    Co-authored-by: Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>

--- components/screens/deliveries/ViewAllDeliveriesScreen.tsx
+++ components/screens/deliveries/ViewAllDeliveriesScreen.tsx
@@ -263,7 +263,7 @@ class ViewAllDeliveriesScreen extends React.Component<Props, State> {
         <View>
           <View style={{ flex: 1, flexDirection: "row", padding: 15 }}>
             <View style={styles.center}>
-              <AsyncButton onPress={async () => { await this.changeItemGroup("previous") }} >
+              <AsyncButton onPress={async () => await this.changeItemGroup("previous")}>
                 <Icon style={styles.red} type="Entypo" name="chevron-left"></Icon>
               </AsyncButton>
             </View>
